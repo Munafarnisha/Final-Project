08bf0d0b6ab1aa82c308f7d4496f06266808e4c7
Munafarnisha Asath <40758208+Munafarnisha@users.noreply.github.com>
2019-08-29 13:36:45 +0530

    Add files via upload

---
+++ Travel/bottom.php
@@ -0,0 +1,62 @@
+<div class="fotter">	
+	 <div class="container">
+		 <div class="fotter-grids">
+			 <div class="col-md-4 fotter-left">
+			 <img style="position:relative;right:20px" src="images/fotterlogo2.png" alt="">
+			 <p style="text-align:justify">I am Munafar, your tour director for your next tour. I have been working as a guide for the last five years with americans, australians and spanish groups around the most important places in World. This blog is for me, but sharing it with you, it is also for you, so you can find useful information for your next tour. This is my wish. </p>
+			 </div>
+			 <div class="col-md-4 fotter-middle">
+				 <h3>Questions About Our Tours?</h3>
+				 <div class="footer-list">
+						<ul>
+                        <li style="text-align:justify"><a href="#"><span></span>
+Our Customer Service Department is always happy to answer any questions you might have. Please visit our Contact Page to contact us or call us at 9779730479.</a></li></ul> </div>
+					<h3>why makemytrip?</h3>
+                        <div class="footer-list">
+                        <ul>
+
+ <li style="text-align:justify"><a href="#"><span></span>Customer Satisfaction<br/></a></li>
+ <li style="text-align:justify"><a href="#"><span></span>Superior customer service, 24x7 Dedicated helpline and over 5 million delighted customers & still growing.<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Best Deals Guaranteed<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Great experiences at lowest prices guaranteed.<br/></a></li>
+</a></li> </ul>
+			 </div>
+             
+			 </div>
+			 <div class="col-md-4 fotter-right" style="padding-left:70px">
+			  <h3>Why buy from us?</h3><br/>
+              <div class="footer-list">
+			  <ul>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><div style="margin-right:110px;">Payment Security</div></a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><div style="margin-right:140px;">Travel Guide</div></a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><div style="margin-right:210px;Width:140px">Social Responsibility</div></a></li>
+               </ul> </div></div>
+			 <div class="social-icons">
+			 <a href="#"><span style="border:2px solid White" class="facebook"> </span></a>
+			 <a href="#"><span style="border:2px solid White" class="twitter"> </span></a>
+			 <a href="#"><span style="border:2px solid White" class="googleplus"> </span></a>
+			 <a href="#"><span style="border:2px solid White" class="pinterest"> </span></a>
+			 <a href="#"><span style="border:2px solid White" class="instagram"> </span></a>
+			 </div>
+			 <div class="clearfix"></div>
+	     </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+</div>  
+
+  <script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.nav.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$('#nav').onePageNav({
+			begin: function() {
+			console.log('start')
+			},
+			end: function() {
+			console.log('stop')
+			}
+		});
+	});
+	</script>
+  
